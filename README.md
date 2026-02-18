6676b10032c958a1d32b8f069519cb0e004c7cce
Luccas Fialho <luccas.flh@gmail.com>
2026-02-18 01:27:09 +0000

    add `lint:check` and `lint:fix` scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 A journey to build an app from scratch
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 function Home() {
-    return <>
-        <h1>Djulia, você é muito importante pra mim! Tô com saudades 💜</h1>
-        <h2>Je t'aime, ma chérie. ❤️</h2>
+  return (
+    <>
+      <h1>Djulia, você é muito importante pra mim! Tô com saudades 💜</h1>
+      <h2>Je t'aime, ma chérie. ❤️</h2>
     </>
-
+  );
 }
 
 export default Home;
