f702170a2f68768a4b0f96d2f09c8b1267f4d394
Luccas Fialho <luccas.flh@gmail.com>
2026-03-26 12:58:03 +0000

    add `/api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,16 +1,10 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV == "development" ? false : true,
-  });
+  let client;
+
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
@@ -21,6 +15,31 @@ async function query(queryObject) {
   }
 }
 
+function getSSLValues() {
+  if (process.env.POSTGRESS_CA) {
+    return {
+      ca: process.env.POSTGRESS_CA,
+    };
+  }
+
+  return process.env.NODE_ENV == "production" ? true : false;
+}
+
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+  return client;
+}
+
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
