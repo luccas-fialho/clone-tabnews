95dd982d7fe15f8394d9c3644e0e841d692a36a9
Luccas Fialho <luccas.flh@gmail.com>
2026-05-08 14:56:20 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,23 +4,27 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET to /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
-  expect(responseBody.dependencies.database).toBeDefined();
+      const responseBody = await response.json();
+      expect(responseBody.updated_at).toBeDefined();
+      expect(responseBody.dependencies.database).toBeDefined();
 
-  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+      const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdateAt);
 
-  expect(responseBody.dependencies.database.version).toBe("16.13");
+      expect(responseBody.dependencies.database.version).toBe("16.13");
 
-  const opennedConnections =
-    responseBody.dependencies.database.openned_connections;
-  const maxConnections = responseBody.dependencies.database.max_connections;
-  expect(maxConnections).toEqual(100);
-  expect(opennedConnections).toEqual(1);
-  expect(opennedConnections).toBeLessThan(maxConnections);
+      const opennedConnections =
+        responseBody.dependencies.database.openned_connections;
+      const maxConnections = responseBody.dependencies.database.max_connections;
+      expect(maxConnections).toEqual(100);
+      expect(opennedConnections).toEqual(1);
+      expect(opennedConnections).toBeLessThan(maxConnections);
+    });
+  });
 });
